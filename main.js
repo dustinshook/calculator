209f1b94178115e4a71e7dfce3f8b9ab18bccc2e
dustin <dustin@fantasycruncher.com>
2023-07-22 17:58:31 -0400

    calculator ui

--- main.js
+++ main.js
@@ -3,10 +3,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const calc_container = document.getElementById('calculator');
 
-    for (let i = 0; i <= 9; i++) {
-        const calc_button = document.createElement('button');
+    for (let i = 9; i >= 0; i--) {
+        const calc_button = document.createElement('div');
         calc_button.classList.add('calc-button');
         calc_button.textContent = i;
         calc_container.appendChild(calc_button);
     }
+
+    const operators = ['.', '+', '-', '*', '/', '='];
+
+    operators.forEach(operator => {
+        const calc_button = document.createElement('div');
+        calc_button.classList.add('calc-button');
+        calc_button.textContent = operator;
+        calc_container.appendChild(calc_button);
+    });
 });
